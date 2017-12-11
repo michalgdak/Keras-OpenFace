419e802ca111c9e7daf4bcb7b4c9ffe6e3a4d3b6
michalgdak <michal.gdak@gmail.com>
2017-12-11 23:31:28 +0100

    Testing

--- train.py
+++ train.py
@@ -2,4 +2,4 @@ from keras.models import load_model
 #from keras.utils import CustomObjectScope
 import tensorflow as tf
 #with CustomObjectScope({'tf': tf}):
-  model = load_model('./model/nn4.small2.v1.h5', custom_objects={'tf':tf})
+model = load_model('./model/nn4.small2.v1.h5', custom_objects={'tf':tf})
